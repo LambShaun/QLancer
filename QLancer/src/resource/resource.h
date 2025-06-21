0fbecbd6ba871a14021b26df27e3574f19273183
LambShaun <Linksff03@gmail.com>
2025-06-21 19:41:39 +0800

    Icon resource and mvp test

---
+++ QLancer/src/resource/resource.h
@@ -0,0 +1,10 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#define IDI_APPICON              101
+#define IDM_TRAY_EXIT            9001
+#define WM_APP_SEARCH_COMPLETE   0x8001
+#define WM_APP_TRAY_MSG          0x8002
+
+#endif // RESOURCE_H
+
